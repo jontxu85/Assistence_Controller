da5f9c7aa7e12ba550a594c3d040f8fb2442ff52
jontxu85 <jonandergonzalez@hotmail.com>
2017-03-09 10:32:41 +0100

    persistance.xml

--- src/main/java/com/proyecto/beans/IndexBean.java
+++ src/main/java/com/proyecto/beans/IndexBean.java
@@ -1,5 +1,47 @@
 package com.proyecto.beans;
 
-public class IndexBean {
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import modelos.Empleado;
+import service.EmpleadoService;
+
+@ManagedBean
+@RequestScoped
+public class IndexBean implements Serializable {
+
+	private static final long serialVersionUID = -4901741514584007341L;
+	private EmpleadoService actorService = new EmpleadoService();
+	private String username;
+	private String password;
+	
+	
+
+	@PostConstruct
+	public void init() {
+		username = "";
+		password = "";
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
 }
